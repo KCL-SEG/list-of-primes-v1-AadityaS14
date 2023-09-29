a98fc5582a486ce737e7cd0cd24e9a47b6f72e27
Aaditya Shivakumar <aaditya.shivakumar@kcl.ac.uk>
2023-09-29 19:59:48 +0100

    Find and return the first n primes

--- primes.py
+++ primes.py
@@ -1,6 +1,18 @@
 """List of prime numbers generator."""
 """ENTER YOUR SOLUTION HERE!"""
+from math import sqrt
 
 def primes(number_of_primes):
     list = []
+    num = 2
+    while len(list) < number_of_primes:
+        if is_prime(num):
+            list.append(num)
+        num += 1
     return list
+
+def is_prime(num):
+    for i in range(2, int(sqrt(num)) + 1):
+        if num % i == 0:
+            return False
+    return True
